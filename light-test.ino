036db4172877f345d554cc19b2c479f2b07cc76d
Peter Golledge <petergolledge@gmail.com>
2022-11-01 09:17:05 -0600

    Got non blocking Waist 90% sorted

--- light-test.ino
+++ light-test.ino
@@ -1,13 +1,31 @@
+// Makes the HourGlass light and "count downdown" with a Arduino Mega
+
+// Set MAX for Mega
+#define FADE_LED_MAX_LED  15
 #include <VariableTimedAction.h>
 #include <FadeLed.h>
 
-// Arrays for Pin assignment
+// FadeLed Arrays for Pin assignment in FadeLED, note #define above
+// FADE_LED_PWM_BITS for a Mega
 
 FadeLed myBotPins[] = { 2, 3, 4, 9, 6, 7, 8 };
 FadeLed myTopPins[] = { 46, 45, 44, 10, 11, 13, 12 };
+
+// Regular array for Waist leds, these are non PWM
 int myWaistPins[] = { 30, 31, 32, 33 };
 
-// Global Pin counter to keep state
+// Keep state for Waist pins
+bool myWaistPinsLit[] = { LOW, LOW, LOW, LOW };
+bool myWaistIncrement = false;
+
+// WaistPin current index value
+int myWaistPin = 0;
+
+// Timer values for Waist LEDS.  Need non-blocking LED blink
+unsigned long previousMillis = 0;  // will store last time LED was updated
+const unsigned long delta = 50;  // interval at which to blink (milliseconds)
+
+// Global Pin counter to keep state, since we timer driven
 int myBarPin = 0;
 // Global Fade values
 bool myFading = true;
@@ -16,21 +34,15 @@ bool myFading = true;
 int sensorPin = A0;   // select the input pin for the potentiometer
 int sensorValue = 0;  // variable to store the value coming from the sensor
 
-// Setup all pins to output
+// Setup NON PWM pins to output, FadeLed takes care of PWM
 void pinSetup() {
   int myPin = 0;
-//  for (myPin = 0; myPin < 7; myPin = myPin + 1) {
-//    pinMode(myTopPins[myPin], OUTPUT);
-//  }
-//  for (myPin = 0; myPin < 7; myPin = myPin + 1) {
-//    pinMode(myBotPins[myPin], OUTPUT);
-//  }
   for (myPin = 0; myPin < 4; myPin = myPin + 1) {
     pinMode(myWaistPins[myPin], OUTPUT);
   }
   
 }
-// Initial debug and then blank bottom pins
+// Light everything for H/W debug and then blank bottom pins
 void barReset() {
   int myPin = 0;
   // Turn on all Pins for debug
@@ -61,17 +73,39 @@ void barReset() {
 
 class WaistBlink : public VariableTimedAction {
 private:
-  int myWaistPin = 0;
   int count = 0;
-
   //Method to cycles Waist pins at specific timing
   unsigned long run() {
-    for (myWaistPin = 0; myWaistPin < 4; myWaistPin = myWaistPin + 1) {
-      digitalWrite(myWaistPins[myWaistPin], HIGH);
-      delay(100);
-      digitalWrite(myWaistPins[myWaistPin], LOW);
-    }
+    unsigned long currentMillis = millis();
+    // If time go flip LEDs
+    if (currentMillis - previousMillis >= delta) {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;
 
+      // if the LED is off turn it on and vice-versa:
+      if (myWaistPinsLit[myWaistPin] == LOW) {
+        myWaistPinsLit[myWaistPin] = HIGH;
+      } else {
+        myWaistPinsLit[myWaistPin] = LOW;
+      }
+      // set the LED with the ledState of the variable:
+      digitalWrite(myWaistPins[myWaistPin], myWaistPinsLit[myWaistPin]);
+
+      // If we need to increment set it
+      if (myWaistIncrement == false) {
+        myWaistIncrement = true;
+        if ( myWaistPin > 2 ) {
+           myWaistPin = 0;
+        }
+        else {
+           myWaistPin++;
+        }
+      }
+      else {
+        myWaistIncrement = false;
+      }
+
+    }
     //return code of 0 indicates no change to the interval
     //if the interval must be changed, then return the new interval
     return 0;
@@ -137,7 +171,7 @@ public:
 };
 
 // Timed event for Waist
-//WaistBlink myWaist;
+WaistBlink myWaist;
 // Timed event for Top and Bottom bars
 BarBlink myBar;
 
@@ -151,9 +185,9 @@ void setup() {
 // Reset bar state  
   barReset();
     
-//  myWaist.start(1000);
+  myWaist.start(100);
   // Start fixed 15 Sec delay, then will reset to whatever Potentiometer says * const
-  myBar.start(1000);
+//  myBar.start(1000);
 }
 
 void loop() {
